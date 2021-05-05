503fb5ca64a15d04ffa01fa085bc70d3a4fd32f0
arkiver <arkiver@protonmail.com>
2021-05-06 00:02:44 +0200

    Version 20210506.01. Initial.

---
+++ liveleak.lua
@@ -0,0 +1,399 @@
+dofile("table_show.lua")
+dofile("urlcode.lua")
+local urlparse = require("socket.url")
+local http = require("socket.http")
+JSON = (loadfile "JSON.lua")()
+
+local item_dir = os.getenv('item_dir')
+local warc_file_base = os.getenv('warc_file_base')
+local item_type = nil
+local item_name = nil
+local item_value = nil
+
+local url_count = 0
+local tries = 0
+local downloaded = {}
+local addedtolist = {}
+local abortgrab = false
+local exitgrab = false
+local exit_url = false
+
+local discovered = {}
+local found_second = false
+
+local bad_items = {}
+
+if not urlparse or not http then
+  io.stdout:write("socket not correctly installed.\n")
+  io.stdout:flush()
+  abortgrab = true
+end
+
+local ids = {}
+
+for ignore in io.open("ignore-list", "r"):lines() do
+  downloaded[ignore] = true
+end
+
+abort_item = function(abort)
+  abort = true
+  if abort then
+    abortgrab = true
+  end
+  exitgrab = true
+  if not bad_items[item_name] then
+    io.stdout:write("Aborting item " .. item_name .. ".\n")
+    io.stdout:flush()
+    bad_items[item_name] = true
+  end
+end
+
+read_file = function(file)
+  if file then
+    local f = assert(io.open(file))
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+submit_discovered = function()
+  local newitems = nil
+  for item, _ in pairs(discovered) do
+    io.stdout:write("Queuing item " .. item .. ".\n")
+    io.stdout:flush()
+    if newitems == nil then
+      newitems = item
+    else
+      newitems = newitems .. "\0" .. item
+    end
+  end
+  if newitems ~= nil then
+    local tries = 0
+    while tries < 10 do
+      local body, code, headers, status = http.request(
+        "http://blackbird-amqp.meo.ws:23038/liveleak-ky9xnba3bqx5s8w/",
+        newitems
+      )
+      if code == 200 or code == 409 then
+        break
+      end
+      os.execute("sleep " .. math.floor(math.pow(2, tries)))
+      tries = tries + 1
+    end
+    if tries == 10 then
+      abort_item()
+    end
+  end
+end
+
+get_item = function(url)
+  local match = string.match(url, "^https?://[^/]*liveleak%.com/view%?[ti]=([0-9a-zA-Z_%-]+_[0-9]+)")
+  local type_ = "video-old"
+  if not match then
+    match = string.match(url, "^https?://[^/]*liveleak%.com/v%?t=([0-9a-zA-Z_%-]+)")
+    type_ = "video-new"
+  end
+  if not match then
+    match = string.match(url, "^https?://[^/]*liveleak%.com/e/([0-9a-zA-Z_%-]+)")
+    type_ = "embed-old"
+    if not match then
+      match = string.match(url, "^https?://[^/]*liveleak%.com/ll_embed%?f=([0-9a-zA-Z_%-]+)")
+    end
+  end
+  if not match then
+    match = string.match(url, "^https?://[^/]*liveleak%.com/e%?t=([0-9a-zA-Z_%-]+)")
+    type_ = "embed-new"
+  end
+  if not match then
+    match = string.match(url, "^https?://[^/]*liveleak.com/list%?q=([^&]+)")
+    type_ = "user"
+  end
+  if match then
+    return type_, match
+  end
+end
+
+allowed = function(url, parenturl)
+  if string.match(url, "language_code=")
+    or string.match(url, "theme_id=") then
+    return false
+  end
+
+  local tested = {}
+  for s in string.gmatch(url, "([^/]+)") do
+    if not tested[s] then
+      tested[s] = 0
+    end
+    if tested[s] == 6 then
+      return false
+    end
+    tested[s] = tested[s] + 1
+  end
+
+  if string.match(url, "^https?://cdn[0-9]*%.liveleak%.com/") then
+    return true
+  end
+
+  local type_, match = get_item(url)
+  if ((type_ == "embed-new" and item_type == "video-new")
+    or (type_ == "video-new" and item_type == "embed-new")
+    or (type_ == "embed-old" and item_type == "video-old")
+    or (type_ == "video-old" and item_type == "embed-old"))
+    and not ids[match]
+    and not found_second then
+    print(type_, item_type, match)
+    ids[match] = true
+    found_second = true
+  elseif match and not ids[match] then
+    discovered[type_ .. ":" .. match] = true
+  end
+
+  for _, p in pairs({"([0-9a-zA-Z_%-]+)", "([0-9]+)", "([^%?&]+)"}) do
+    for s in string.gmatch(url, p) do
+      if ids[s] then
+        return true
+      end
+    end
+  end
+
+  match = string.match(url, "/c/([^%?&]+)")
+  if match then
+    discovered["user:" .. match] = true
+  end
+
+  return false
+end
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  return false
+end
+
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  local urls = {}
+  local html = nil
+
+  if is_css then
+    return urls
+  end
+  
+  downloaded[url] = true
+
+  local function check(urla)
+    local origurl = url
+    local url = string.match(urla, "^([^#]+)")
+    local url_ = string.match(url, "^(.-)%.?$")
+    url_ = string.gsub(url_, "&amp;", "&")
+    url_ = string.gsub(url_, "\\[uU]([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F])", function (s)
+      local i = tonumber(s, 16)
+      if i < 128 then
+        return string.char(i)
+      else
+        -- should not have these
+        abort_item()
+      end
+    end)
+    url_ = string.match(url_, "^(.-)%s*$")
+    url_ = string.match(url_, "^(.-)%??$")
+    url_ = string.match(url_, "^(.-)&?$")
+    --url_ = string.match(url_, "^(.-)/?$")
+    url_ = string.match(url_, "^(.-)\\?$")
+    if (downloaded[url_] ~= true and addedtolist[url_] ~= true)
+      and allowed(url_, origurl) then
+      table.insert(urls, { url=url_ })
+      addedtolist[url_] = true
+      addedtolist[url] = true
+    end
+  end
+
+  local function checknewurl(newurl)
+    if string.match(newurl, "^/>")
+      or string.match(newurl, "^/&gt;")
+      or string.match(newurl, "^/<")
+      or string.match(newurl, "^/&lt;")
+      or string.match(newurl, "^/%*") then
+      return false
+    end
+    if string.match(newurl, "\\[uU]002[fF]") then
+      return checknewurl(string.gsub(newurl, "\\[uU]002[fF]", "/"))
+    end
+    if string.match(newurl, "^https?:////") then
+      check(string.gsub(newurl, ":////", "://"))
+    elseif string.match(newurl, "^https?://") then
+      check(newurl)
+    elseif string.match(newurl, "^https?:\\/\\?/") then
+      check(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^\\/") then
+      checknewurl(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^//") then
+      check(urlparse.absolute(url, newurl))
+    elseif string.match(newurl, "^/") then
+      check(urlparse.absolute(url, newurl))
+    elseif string.match(newurl, "^%.%./") then
+      if string.match(url, "^https?://[^/]+/[^/]+/") then
+        check(urlparse.absolute(url, newurl))
+      else
+        checknewurl(string.match(newurl, "^%.%.(/.+)$"))
+      end
+    elseif string.match(newurl, "^%./") then
+      check(urlparse.absolute(url, newurl))
+    end
+  end
+
+  local function checknewshorturl(newurl)
+    checknewurl(newurl)
+    if string.match(newurl, "^%?") then
+      check(urlparse.absolute(url, newurl))
+    elseif not (string.match(newurl, "^https?:\\?/\\?//?/?")
+      or string.match(newurl, "^[/\\]")
+      or string.match(newurl, "^%./")
+      or string.match(newurl, "^[jJ]ava[sS]cript:")
+      or string.match(newurl, "^[mM]ail[tT]o:")
+      or string.match(newurl, "^vine:")
+      or string.match(newurl, "^android%-app:")
+      or string.match(newurl, "^ios%-app:")
+      or string.match(newurl, "^%${")) then
+      check(urlparse.absolute(url, newurl))
+    end
+  end
+
+  local type_, match = get_item(url)
+  if type_ == "video-old" then
+    check(string.gsub(url, "/view%?[ti]=", "/view%?t="))
+    check(string.gsub(url, "/view%?[ti]=", "/view%?i="))
+  elseif type_ == "embed-old" then
+    check(string.gsub(url, "/e/", "/ll_embed%?f="))
+    check(string.gsub(url, "/ll_embed%?f=", "/e/"))
+  end
+
+  if allowed(url, nil) and status_code == 200
+    and not string.match(url, "^https?://cdn[0-9]*%.liveleak%.com/") then
+    html = read_file(file)
+    for user in string.gmatch(html, "Credit: ([^<%s]+)%s+<") do
+      discovered["user:" .. user] = true
+    end
+    for newurl in string.gmatch(string.gsub(html, "&quot;", '"'), '([^"]+)') do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(string.gsub(html, "&#039;", "'"), "([^']+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, ">%s*([^<%s]+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "[^%-]href='([^']+)'") do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, '[^%-]href="([^"]+)"') do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, ':%s*url%(([^%)"]+)%)') do
+      checknewurl(newurl)
+    end
+  end
+
+  return urls
+end
+
+set_new_item = function(url)
+  type_, match = get_item(url)
+  if match and not ids[match] then
+    abortgrab = false
+    exitgrab = false
+    ids[match] = true
+    item_value = match
+    item_type = type_
+    item_name = type_ .. ":" .. match
+    io.stdout:write("Archiving item " .. item_name .. ".\n")
+    io.stdout:flush()
+  end
+end
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  status_code = http_stat["statcode"]
+
+  if abortgrab or exitgrab then
+    abort_item(true)
+    submit_discovered()
+    return wget.actions.ABORT
+    --return wget.actions.EXIT
+  end
+
+  set_new_item(url["url"])
+  
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. "  \n")
+  io.stdout:flush()
+
+  if exitgrab then
+    return wget.actions.EXIT
+  end
+
+  if status_code == 404 and string.match(url["url"], "%.mp4") then
+    abort_item(true)
+    submit_discovered()
+    return wget.actions.ABORT
+  end
+
+  if status_code >= 300 and status_code <= 399 then
+    local newloc = urlparse.absolute(url["url"], http_stat["newloc"])
+    if downloaded[newloc] or addedtolist[newloc] then
+      tries = 0
+      return wget.actions.EXIT
+    end
+  end
+  
+  if status_code >= 200 and status_code <= 399 then
+    downloaded[url["url"]] = true
+  end
+
+  if status_code == 0
+    or (status_code >= 400 and status_code ~= 404) then
+    io.stdout:write("Server returned " .. http_stat.statcode .. " (" .. err .. "). Sleeping.\n")
+    io.stdout:flush()
+    local maxtries = 1
+    if tries >= maxtries then
+      io.stdout:write("I give up...\n")
+      io.stdout:flush()
+      tries = 0
+      abort_item(true)
+      submit_discovered()
+      return wget.actions.ABORT
+    else
+      os.execute("sleep " .. math.floor(math.pow(2, tries)))
+      tries = tries + 1
+      return wget.actions.CONTINUE
+    end
+  end
+
+  tries = 0
+
+  local sleep_time = 0
+
+  if sleep_time > 0.001 then
+    os.execute("sleep " .. sleep_time)
+  end
+
+  return wget.actions.NOTHING
+end
+
+wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total_downloaded_bytes, total_download_time)
+  local file = io.open(item_dir .. '/' .. warc_file_base .. '_bad-items.txt', 'w')
+  for item, _ in pairs(bad_items) do
+    file:write(item .. "\n")
+  end
+  file:close()
+  submit_discovered()
+end
+
+wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  submit_discovered()
+  if abortgrab then
+    abort_item(true)
+    return wget.exits.IO_FAIL
+  end
+  return exit_status
+end
+
---
+++ pipeline.py
@@ -0,0 +1,381 @@
+# encoding=utf8
+import datetime
+import distutils.version
+import functools
+import hashlib
+import os
+import random
+import re
+import shutil
+import socket
+import string
+import subprocess
+import sys
+import time
+
+import requests
+from seesaw.config import realize, NumberConfigValue
+from seesaw.externalprocess import ExternalProcess, RsyncUpload
+from seesaw.item import ItemInterpolation, ItemValue
+from seesaw.task import SimpleTask, LimitConcurrent, Task
+from seesaw.tracker import GetItemFromTracker, PrepareStatsForTracker, \
+    UploadWithTracker, SendDoneToTracker
+import seesaw
+from seesaw.externalprocess import WgetDownload
+from seesaw.pipeline import Pipeline
+from seesaw.project import Project
+from seesaw.util import find_executable
+
+if distutils.version.StrictVersion(seesaw.__version__) < distutils.version.StrictVersion('0.8.5'):
+    raise Exception('This pipeline needs seesaw version 0.8.5 or higher.')
+
+
+###########################################################################
+# Find a useful Wget+Lua executable.
+#
+# WGET_AT will be set to the first path that
+# 1. does not crash with --version, and
+# 2. prints the required version string
+
+WGET_AT = find_executable(
+    'Wget+AT',
+    ['GNU Wget 1.20.3-at.20210410.01'],
+    [
+        './wget-at',
+        '/home/warrior/data/wget-at'
+    ]
+)
+
+if not WGET_AT:
+    raise Exception('No usable Wget+At found.')
+
+
+###########################################################################
+# The version number of this pipeline definition.
+#
+# Update this each time you make a non-cosmetic change.
+# It will be added to the WARC files and reported to the tracker.
+VERSION = '20210506.01'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:52.0) Gecko/20100101 Firefox/59.0'
+TRACKER_ID = 'liveleak'
+TRACKER_HOST = 'legacy-api.arpa.li'
+MULTI_ITEM_SIZE = 1 # keep 1
+KEEP_WARC_ON_ABORT = False
+
+
+###########################################################################
+# This section defines project-specific tasks.
+#
+# Simple tasks (tasks that do not need any concurrency) are based on the
+# SimpleTask class and have a process(item) method that is called for
+# each item.
+class CheckIP(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, 'CheckIP')
+        self._counter = 0
+
+    def process(self, item):
+        # NEW for 2014! Check if we are behind firewall/proxy
+
+        if self._counter <= 0:
+            item.log_output('Checking IP address.')
+            ip_set = set()
+
+            ip_set.add(socket.gethostbyname('twitter.com'))
+            ip_set.add(socket.gethostbyname('facebook.com'))
+            ip_set.add(socket.gethostbyname('youtube.com'))
+            ip_set.add(socket.gethostbyname('microsoft.com'))
+            ip_set.add(socket.gethostbyname('icanhas.cheezburger.com'))
+            ip_set.add(socket.gethostbyname('archiveteam.org'))
+
+            if len(ip_set) != 6:
+                item.log_output('Got IP addresses: {0}'.format(ip_set))
+                item.log_output(
+                    'Are you behind a firewall/proxy? That is a big no-no!')
+                raise Exception(
+                    'Are you behind a firewall/proxy? That is a big no-no!')
+
+        # Check only occasionally
+        if self._counter <= 0:
+            self._counter = 10
+        else:
+            self._counter -= 1
+
+
+class PrepareDirectories(SimpleTask):
+    def __init__(self, warc_prefix):
+        SimpleTask.__init__(self, 'PrepareDirectories')
+        self.warc_prefix = warc_prefix
+
+    def process(self, item):
+        item_name = item['item_name']
+        item_name_hash = hashlib.sha1(item_name.encode('utf8')).hexdigest()
+        escaped_item_name = item_name_hash
+        dirname = '/'.join((item['data_dir'], escaped_item_name))
+
+        if os.path.isdir(dirname):
+            shutil.rmtree(dirname)
+
+        os.makedirs(dirname)
+
+        item['item_dir'] = dirname
+        item['warc_file_base'] = '-'.join([
+            self.warc_prefix,
+            item_name_hash,
+            time.strftime('%Y%m%d-%H%M%S')
+        ])
+
+        open('%(item_dir)s/%(warc_file_base)s.warc.gz' % item, 'w').close()
+        open('%(item_dir)s/%(warc_file_base)s_bad-items.txt' % item, 'w').close()
+        open('%(item_dir)s/%(warc_file_base)s_data.txt' % item, 'w').close()
+
+class MoveFiles(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, 'MoveFiles')
+
+    def process(self, item):
+        os.rename('%(item_dir)s/%(warc_file_base)s.warc.gz' % item,
+              '%(data_dir)s/%(warc_file_base)s.warc.gz' % item)
+        os.rename('%(item_dir)s/%(warc_file_base)s_data.txt' % item,
+              '%(data_dir)s/%(warc_file_base)s_data.txt' % item)
+
+        shutil.rmtree('%(item_dir)s' % item)
+
+
+class ChooseTargetAndUpload(Task):
+    def __init__(self):
+        Task.__init__(self, 'ChooseTargetAndUpload')
+        self.retry_sleep = 10
+
+    def enqueue(self, item):
+        self.start_item(item)
+        item.log_output('Starting %s for %s\n' % (self, item.description()))
+        self.process(item)
+
+    def process(self, item):
+        try:
+            target = self.find_target(item)
+            assert target is not None
+        except:
+            item.log_output('Could not get rsync target.')
+            return self.retry(item)
+        inner_task = RsyncUpload(
+            target,
+            [
+                '%(data_dir)s/%(warc_file_base)s.warc.gz' % item,
+                '%(data_dir)s/%(warc_file_base)s_data.txt' % item
+            ],
+            target_source_path='%(data_dir)s/' % item,
+            extra_args=[
+                '--recursive',
+                '--partial',
+                '--partial-dir', '.rsync-tmp',
+                '--min-size', '1',
+                '--no-compress',
+                '--compress-level', '0'
+            ],
+            max_tries=1
+        )
+        inner_task.on_complete_item = lambda task, item: self.complete_item(item)
+        inner_task.on_fail_item = lambda task, item: self.retry(item)
+        inner_task.enqueue(item)
+
+    def retry(self, item):
+        item.log_output('Failed to upload, retrying...')
+        IOLoop.instance().add_timeout(
+            datetime.timedelta(seconds=self.retry_sleep),
+            functools.partial(self.process, item)
+        )
+
+    def find_target(self, item):
+        item.log_output('Requesting targets.')
+        r = requests.get('https://{}/{}/upload_targets'
+                         .format(TRACKER_HOST, TRACKER_ID))
+        targets = r.json()
+        random.shuffle(targets)
+        for target in targets:
+            item.log_output('Trying target {}.'.format(target))
+            domain = re.search('^[^:]+://([^/:]+)', target).group(1)
+            size = os.path.getsize(
+                '%(data_dir)s/%(warc_file_base)s.warc.gz' % item
+            )
+            r = requests.get(
+                'http://{}:3002/'.format(domain),
+                params={
+                    'name': item['item_name'],
+                    'size': size
+                },
+                timeout=3
+            )
+            if r.json()['accepts']:
+                item.log_output('Picking target {}.'.format(target))
+                return target.replace(':downloader', item['stats']['downloader'])
+        else:
+            item.log_output('Could not find a target.')
+
+
+class SetBadUrls(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, 'SetBadUrls')
+
+    def process(self, item):
+        item['item_name_original'] = item['item_name']
+        items = item['item_name'].split('\0')
+        items_lower = [s.lower() for s in items]
+        with open('%(item_dir)s/%(warc_file_base)s_bad-items.txt' % item, 'r') as f:
+            for aborted_item in f:
+                aborted_item = aborted_item.strip().lower()
+                index = items_lower.index(aborted_item)
+                item.log_output('Item {} is aborted.'.format(aborted_item))
+                items.pop(index)
+                items_lower.pop(index)
+        item['item_name'] = '\0'.join(items)
+
+
+class MaybeUploadWithTracker(UploadWithTracker):
+    def enqueue(self, item):
+        if len(item['item_name']) == 0 and not KEEP_WARC_ON_ABORT:
+            item.log_output('Skipping UploadWithTracker.')
+            return self.complete_item(item)
+        return super(UploadWithTracker, self).enqueue(item)
+
+
+class MaybeSendDoneToTracker(SendDoneToTracker):
+    def enqueue(self, item):
+        if len(item['item_name']) == 0:
+            item.log_output('Skipping SendDoneToTracker.')
+            return self.complete_item(item)
+        return super(MaybeSendDoneToTracker, self).enqueue(item)
+
+
+def get_hash(filename):
+    with open(filename, 'rb') as in_file:
+        return hashlib.sha1(in_file.read()).hexdigest()
+
+CWD = os.getcwd()
+PIPELINE_SHA1 = get_hash(os.path.join(CWD, 'pipeline.py'))
+LUA_SHA1 = get_hash(os.path.join(CWD, 'liveleak.lua'))
+
+def stats_id_function(item):
+    d = {
+        'pipeline_hash': PIPELINE_SHA1,
+        'lua_hash': LUA_SHA1,
+        'python_version': sys.version,
+    }
+
+    return d
+
+
+class WgetArgs(object):
+    def realize(self, item):
+        wget_args = [
+            WGET_AT,
+            '-U', USER_AGENT,
+            '-nv',
+            '--content-on-error',
+            '--lua-script', 'liveleak.lua',
+            '-o', ItemInterpolation('%(item_dir)s/wget.log'),
+            '--no-check-certificate',
+            '--output-document', ItemInterpolation('%(item_dir)s/wget.tmp'),
+            '--truncate-output',
+            '-e', 'robots=off',
+            '--rotate-dns',
+            '--recursive', '--level=inf',
+            '--page-requisites',
+            '--timeout', '30',
+            '--tries', 'inf',
+            '--domains', 'liveleak.com',
+            '--span-hosts',
+            '--waitretry', '30',
+            '--warc-file', ItemInterpolation('%(item_dir)s/%(warc_file_base)s'),
+            '--warc-header', 'operator: Archive Team',
+            '--warc-header', 'x-wget-at-project-version: ' + VERSION,
+            '--warc-header', 'x-wget-at-project-name: ' + TRACKER_ID,
+            '--warc-dedup-url-agnostic',
+        ]
+
+        for item_name in item['item_name'].split('\0'):
+            wget_args.extend(['--warc-header', 'x-wget-at-project-item-name: '+item_name])
+            wget_args.append('item-name://'+item_name)
+            item_type, item_value = item_name.split(':', 1)
+            if item_type == 'video-old':
+                wget_args.extend(['--warc-header', 'liveleak-video-id: '+item_value])
+                wget_args.append('https://www.liveleak.com/view?t='+item_value)
+                wget_args.append('https://www.liveleak.com/view?i='+item_value)
+            elif item_type == 'video-new':
+                wget_args.extend(['--warc-header', 'liveleak-video-id: '+item_value])
+                wget_args.append('https://www.liveleak.com/v?t='+item_value)
+            elif item_type == 'embed-old':
+                wget_args.extend(['--warc-header', 'liveleak-video-embed: '+item_value])
+                wget_args.append('https://www.liveleak.com/ll_embed?f='+item_value)
+                wget_args.append('https://www.liveleak.com/e/'+item_value)
+            elif item_type == 'embed-new':
+                wget_args.extend(['--warc-header', 'liveleak-video-embed: '+item_value])
+                wget_args.append('https://www.liveleak.com/e?t='+item_value)
+            elif item_type == 'user':
+                wget_args.extend(['--warc-header', 'liveleak-user: '+item_value])
+                wget_args.append('https://www.liveleak.com/list?q='+item_value)
+            else:
+                raise ValueError('item_type not supported.')
+
+        item['item_name_newline'] = item['item_name'].replace('\0', '\n')
+
+        if 'bind_address' in globals():
+            wget_args.extend(['--bind-address', globals()['bind_address']])
+            print('')
+            print('*** Wget will bind address at {0} ***'.format(
+                globals()['bind_address']))
+            print('')
+
+        return realize(wget_args, item)
+
+###########################################################################
+# Initialize the project.
+#
+# This will be shown in the warrior management panel. The logo should not
+# be too big. The deadline is optional.
+project = Project(
+    title = 'LiveLeak',
+    project_html = '''
+    <img class="project-logo" alt="logo" src="https://wiki.archiveteam.org/images/c/c8/Liveleak_logo.png" height="50px"/>
+    <h2>liveleak.com <span class="links"><a href="https://liveleak.com/">Website</a> &middot; <a href="http://tracker.archiveteam.org/liveleak/">Leaderboard</a></span></h2>
+    '''
+)
+
+pipeline = Pipeline(
+    CheckIP(),
+    GetItemFromTracker('http://{}/{}/multi={}/'
+        .format(TRACKER_HOST, TRACKER_ID, MULTI_ITEM_SIZE),
+        downloader, VERSION),
+    PrepareDirectories(warc_prefix='liveleak'),
+    WgetDownload(
+        WgetArgs(),
+        max_tries=1,
+        accept_on_exit_code=[0, 4, 8],
+        env={
+            'item_dir': ItemValue('item_dir'),
+            'warc_file_base': ItemValue('warc_file_base'),
+        }
+    ),
+    SetBadUrls(),
+    PrepareStatsForTracker(
+        defaults={'downloader': downloader, 'version': VERSION},
+        file_groups={
+            'data': [
+                ItemInterpolation('%(item_dir)s/%(warc_file_base)s.warc.gz')
+            ]
+        },
+        id_function=stats_id_function,
+    ),
+    MoveFiles(),
+    LimitConcurrent(NumberConfigValue(min=1, max=20, default='2',
+        name='shared:rsync_threads', title='Rsync threads',
+        description='The maximum number of concurrent uploads.'),
+        ChooseTargetAndUpload(),
+    ),
+    MaybeSendDoneToTracker(
+        tracker_url='http://%s/%s' % (TRACKER_HOST, TRACKER_ID),
+        stats=ItemValue('stats')
+    )
+)
+
